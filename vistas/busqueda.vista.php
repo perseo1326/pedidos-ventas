0bfaf219ff423527bee66179e678cf571256b0bf
Jwsk <perseo1326@gmail.com>
2021-05-21 20:38:08 -0500

    pagina de busqueda

--- vistas/busqueda.vista.php
+++ vistas/busqueda.vista.php
@@ -2,31 +2,32 @@
 
 ?>
 
-<div class="flex-container">
-    <div class="flex-container ancho-100">
-        <h1 class="margen-1 txt-centro txt-big ancho-100">Búsqueda de Pedido</h1>
-        <form action="" method="post">
-        <div class="flex-container">
-            <div class="flex-container ancho-50">
+<div class="ancho-100 padding-05">
+    <h1 class="ancho-100 padding-05 txt-centro txt-big">Búsqueda de Pedido</h1>
+    <form action="" method="post">
+        <div id="uno" class="flex-container ancho-100">
+            <div class="ancho-50 margen-05">
                 <label for="numPedido" class="ancho-100">Pedido No.</label>
                 <input type="text" name="numPedido" id="" class="ancho-100" placeholder="Número de pedido">
             </div>
-            <div class="flex-container ancho-50">
+            <div class="ancho-45 margen-05 fondo-error">
                 <label for="nombre" class="ancho-100">Nombre</label>
-                <input type="text" name="nombre" id="" class="ancho-100" placeholder="Nombre">
+                <input type="text" name="nombre" id="" class="ancho-100" placeholder="Nombre" style="background-color: transparent;">
             </div>
+        </div>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio corrupti sunt delectus deserunt officia praesentium maiores quo quaerat esse at.</p>
 
+        <br>
+        <br>
+        <br>
             telefono
             fecha
             precio
 
-        </div>
 
             <!-- <input type="button" value="Buscar" class="padding-05"> -->
-        </form>
-    </div>
+        </div>
+    </form>
     <div class="flex-container ancho-100">
         <h2 class="margen-05 txt-centro txt-big ancho-100">Resultados</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus amet necessitatibus labore repellendus vero aut soluta libero, ipsam consequuntur, qui, magni inventore laudantium tempora reiciendis sed dicta saepe perferendis debitis.</p>
