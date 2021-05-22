b6d1f0560ea92ed4ba6e49110b51d6330559f9b4
Jwsk <perseo1326@gmail.com>
2021-05-21 21:09:05 -0500

    busqueda vista

--- vistas/busqueda.vista.php
+++ vistas/busqueda.vista.php
@@ -4,32 +4,41 @@
 
 <div class="ancho-100 padding-05">
     <h1 class="ancho-100 padding-05 txt-centro txt-big">Búsqueda de Pedido</h1>
-    <form action="" method="post">
+    <form class="" action="" method="post">
         <div id="uno" class="flex-container ancho-100">
-            <div class="ancho-50 margen-05">
+            <div class="ancho-25 padding-05">
                 <label for="numPedido" class="ancho-100">Pedido No.</label>
-                <input type="text" name="numPedido" id="" class="ancho-100" placeholder="Número de pedido">
+                <input type="text" name="numPedido" id="" class="ancho-100" placeholder="Pedido #">
             </div>
-            <div class="ancho-45 margen-05 fondo-error">
+            <div class="ancho-75 padding-05">
                 <label for="nombre" class="ancho-100">Nombre</label>
-                <input type="text" name="nombre" id="" class="ancho-100" placeholder="Nombre" style="background-color: transparent;">
+                <input type="text" name="nombre" id="" class="ancho-100" placeholder="Nombre">
             </div>
         </div>
-
-
-        <br>
-        <br>
-        <br>
-            telefono
-            fecha
-            precio
-
-
-            <!-- <input type="button" value="Buscar" class="padding-05"> -->
+        <div id="dos" class="flex-container ancho-100">
+            <div class="ancho-25 padding-05">
+                <label for="nombre" class="ancho-100">Precio</label>
+                <input type="text" name="nombre" id="" class="ancho-100" placeholder="$">
+            </div>
+            <div class="ancho-25 padding-05">
+                <label for="numPedido" class="ancho-100">No. Teléfono</label>
+                <input type="text" name="numPedido" id="" class="ancho-100" placeholder="Teléfono">
+            </div>
+            <div class="ancho-50 padding-05">
+                <label for="nombre" class="ancho-100">Fecha</label>
+                <input type="text" name="nombre" id="" class="ancho-100" placeholder="Fecha">
+            </div>
+        </div>
+        <div id="dos" class="flex-container ancho-100 flex-centrar">
+            <input class="ancho-50 padding-05" type="button" value="Buscar">
         </div>
     </form>
+
+    
     <div class="flex-container ancho-100">
         <h2 class="margen-05 txt-centro txt-big ancho-100">Resultados</h2>
+        <div class="flex-container ancho-100">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus amet necessitatibus labore repellendus vero aut soluta libero, ipsam consequuntur, qui, magni inventore laudantium tempora reiciendis sed dicta saepe perferendis debitis.</p>
+        </div>
     </div>
 </div>
