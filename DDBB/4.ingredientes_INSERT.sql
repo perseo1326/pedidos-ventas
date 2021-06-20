d82351a5e694b46545a0f121b406cab6cd5c8528
Jwsk <perseo1326@gmail.com>
2021-06-20 13:51:03 -0500

    DDBB acentos + tipo usuario + usuario vendedor

--- DDBB/4.ingredientes_INSERT.sql
+++ DDBB/4.ingredientes_INSERT.sql
@@ -17,16 +17,16 @@ INSERT INTO
     `ingred_imagen`,
     `ingred_status`
   ) VALUES
-('indef', 'indef', 'Indefinido', 'Indefinido o no conocido', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'indef'), 'NULL', 'A'), 
+  ('indef', 'indef', 'Indefinido', 'Indefinido o no conocido', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'indef'), 'NULL', 'A'), 
 ('pedido', 'pollo', 'Pollo', 'Pollo deshebrado para rellenos de los panuchos.', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'und'), 'pollo.png', 'A'), 
 ('pedido', 'cerdo', 'Cerdo', 'Cerdo deshebrado para rellenos de los panuchos', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'und'), 'cerdo_face.png', 'A'), 
 ('pedido', 'res', 'Res', 'Res deshebrado para rellenos de los panuchos', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'und'), 'res.png', 'A'), 
 ('pedido', 'queso', 'Queso', 'Queso Manchego y Hebra para relleno de panuchos', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'und'), 'queso.png', 'A'), 
 ('pedido', 'bola', 'Q. Bola', 'Queso de Bola para rellono de panuchos', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'und'), 'bola.png', 'A'), 
-('pedido', 'camaron', 'Camarón', 'Camaron para relleno de panuchos', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'und'), 'camaron.png', 'A'), 
+('pedido', 'camaron', 'Camaron', 'Camaron para relleno de panuchos', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'und'), 'camaron.png', 'A'), 
 ('pedido', 'frijol', 'Frijol', 'Frijol refrito para relleno de panuchos', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'und'), 'frijol.png', 'A'), 
-('pedido', 'azucar', 'Azúcar', 'Mezcla de azúcar y queso para relleno de panuchos', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'und'), 'sugar_cube.png', 'A'), 
-('pedido', 'manual', 'Manual', 'Pedido de edicion manual', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'indef'), 'manual.png', 'A');
+('pedido', 'azucar', 'Azucar', 'Mezcla de azúcar y queso para relleno de panuchos', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'und'), 'sugar_cube.png', 'A'), 
+('pedido', 'manual', 'Manual', 'Pedido de edicion manual', (SELECT presentacion_id FROM presentacion WHERE presentacion_codigo = 'indef'), 'manual.png', 'N'); 
 
 /*!40000 ALTER TABLE `ingredientes` ENABLE KEYS */;
 
--- DDBB/6.tipousuario_INSERT.sql
+++ DDBB/6.tipousuario_INSERT.sql
@@ -9,14 +9,15 @@ DELETE FROM `tipo_usuario`;
 
 
 
-INSERT INTO `tipo_usuario` (`tipousuario_id`, `tipousuario_codigo`, `tipousuario_nombre`, `tipousuario_notas`, `tipousuario_status`) VALUES
-	(1, 'admin', 'administrador', 'usuario general con todos los privilegios.', 'A');
+INSERT INTO `tipo_usuario` (`tipousuario_id`, `tipousuario_codigo`, `tipousuario_nombre`, `tipousuario_notas`, `tipousuario_status`) VALUES 
+(1001, 'admin', 'administrador', 'usuario general con todos los privilegios.'),
+(1002, 'vendedor', 'vendedor', 'Usuario registrado para poder realizar ventas.');
+
 /*!40000 ALTER TABLE `tipo_usuario` ENABLE KEYS */;
-
-
 
 INSERT INTO usuarios (usuarios_nombre, usuarios_descripcion, usuarios_status, usuarios_tipousuario_id)
 VALUES 
-  ('JWSK', '', 'A', 1);
+('JWSK', 'Adminstrador', 'A', (SELECT tipousuario_id FROM tipo_usuario WHERE tipousuario_codigo = 'admin')),
+('Yola', 'Adminstrador', 'A', (SELECT tipousuario_id FROM tipo_usuario WHERE tipousuario_codigo = 'vendedor'));
 
 SET  SQL_SAFE_UPDATES = 1;
